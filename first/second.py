3fa33a06753e3d8b44cf3fbed5580a58499373d3
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-04-14 23:59:11 +0900

    second

---
+++ first/second.py
@@ -0,0 +1,19 @@
+'''
+다음은 삼각형의 밑변의 길이와 높이를 이용하여 삼각형의 면적을 구하는
+프로그램이다. 올바를 실행 결과가 나오도록 프로그램을 완성하시오.
+
+변수명 - 밑변(width), 높이(height), 면적(area)
+width = 100
+height = 20
+
+실행결과 
+밑변 100
+높이 20
+면적 1000
+'''
+width = 100
+height = 20
+area = width * height
+print(width)
+print(height)
+print(area/2)
