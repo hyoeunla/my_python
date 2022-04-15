a46fe0ce5508474ba146a22248bd1eafaaa57f33
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-04-15 23:57:18 +0900

    price

--- first/second.py
+++ first/second.py
@@ -9,7 +9,7 @@ height = 20
 실행결과 
 밑변 100
 높이 20
-면적 1000
+면적 1000.0
 '''
 width = 100
 height = 20
@@ -17,3 +17,22 @@ area = width * height
 print(width)
 print(height)
 print(area/2)
+
+'''
+다음은 물건 가격, 구매개수, 지불금액에 따라 거스름돈을 계산하는 프로그램이다.
+올바른 실행 결과가 나오도록 프로그램을 완성하시오.
+
+실행결과
+물건가격 : 800원
+물건 개수 : 3개
+지불금액 : 5000원
+거스름돈 : 2600원
+'''
+price = 800
+buy = 3
+pay = 5000
+change = pay - price * buy
+print('물건가격 :', price, '원')
+print('구매개수 :', buy, '개')
+print('지불금액 :', price, '원')
+print('거스름돈 :', change, '원')
